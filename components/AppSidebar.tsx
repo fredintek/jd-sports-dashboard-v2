dca0be6beb7b355c5e9b22862abc149b7ab7352e
Alfred Arinze <fredintek@gmail.com>
2025-09-17 16:17:29 +0300

    project initialization

---
+++ components/AppSidebar.tsx
@@ -0,0 +1,244 @@
+"use client";
+import {
+  AlignVerticalSpaceBetween,
+  BookImage,
+  BrickWall,
+  ChevronDown,
+  Eclipse,
+  Eye,
+  Footprints,
+  Frame,
+  HatGlasses,
+  Images,
+  LayoutDashboard,
+  Link2,
+  LocateFixed,
+  Megaphone,
+  Newspaper,
+  NotepadTextDashed,
+  QrCode,
+  ShoppingBag,
+  ShoppingCart,
+  Target,
+  Transgender,
+  WalletCards,
+  Waypoints,
+} from "lucide-react";
+import React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  useSidebar,
+} from "./ui/sidebar";
+import Link from "next/link";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+
+type Props = {};
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Header",
+    url: "#",
+    icon: AlignVerticalSpaceBetween,
+    children: [
+      {
+        title: "Logo",
+        url: "#",
+        icon: Frame,
+      },
+      {
+        title: "Navigation",
+        url: "#",
+        icon: LocateFixed,
+      },
+      {
+        title: "Promo Banner",
+        url: "#",
+        icon: Waypoints,
+      },
+    ],
+  },
+  {
+    title: "Hero",
+    url: "#",
+    icon: Images,
+  },
+  {
+    title: "Brands",
+    url: "#",
+    icon: Target,
+  },
+  {
+    title: "Cards",
+    url: "#",
+    icon: WalletCards,
+    children: [
+      {
+        title: "Gender",
+        url: "#",
+        icon: Transgender,
+      },
+      {
+        title: "Shop Now",
+        url: "#",
+        icon: ShoppingBag,
+      },
+      {
+        title: "Looks",
+        url: "#",
+        icon: Eclipse,
+      },
+    ],
+  },
+  {
+    title: "Gallery",
+    url: "#",
+    icon: BookImage,
+    children: [
+      {
+        title: "Showcase",
+        url: "#",
+        icon: Eye,
+      },
+      {
+        title: "Social Media",
+        url: "#",
+        icon: QrCode,
+      },
+    ],
+  },
+  {
+    title: "Footer",
+    url: "#",
+    icon: Footprints,
+    children: [
+      {
+        title: "Icons",
+        url: "#",
+        icon: BrickWall,
+      },
+      {
+        title: "Newsletter",
+        url: "#",
+        icon: Newspaper,
+      },
+      {
+        title: "Privacy Info",
+        url: "#",
+        icon: HatGlasses,
+      },
+      {
+        title: "Links",
+        url: "#",
+        icon: Link2,
+      },
+      {
+        title: "Advertisement",
+        url: "#",
+        icon: Megaphone,
+      },
+    ],
+  },
+];
+
+const AppSidebar = (props: Props) => {
+  const { open } = useSidebar();
+
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="py-4 mt-3">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <Link href="/">
+                {open ? (
+                  <div className={``}>
+                    <img
+                      src="https://www.jdsports.cy/themes/sleedex-jdsports/img/jd-desktop-logo.png"
+                      alt=""
+                      className="w-full h-full object-contain"
+                    />
+                  </div>
+                ) : (
+                  <div className={`w-[20px] h-[20px]`}>
+                    <img
+                      src="https://www.jdsports.cy/themes/sleedex-jdsports/img/jd-mobile-logo.png"
+                      alt=""
+                      className="w-full h-full object-contain"
+                    />
+                  </div>
+                )}
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarContent className="px-2 mt-6">
+        <SidebarMenu className="gap-3">
+          {items?.map((item) => {
+            if (item.children) {
+              return (
+                <Collapsible key={item.title} className="group/collapsible">
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton>
+                        <item.icon />
+                        <span>{item.title}</span>
+                        <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                      </SidebarMenuButton>
+                    </CollapsibleTrigger>
+
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.children?.map((child) => (
+                          <SidebarMenuSubItem key={child.title}>
+                            <SidebarMenuSubButton asChild>
+                              <Link href={child.url}>
+                                <child.icon />
+                                <span>{child.title}</span>
+                              </Link>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
+              );
+            } else {
+              return (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              );
+            }
+          })}
+        </SidebarMenu>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
---
+++ components/Navbar.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
+import { SidebarTrigger } from "./ui/sidebar";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      {/* left */}
+      <SidebarTrigger />
+      {/* right */}
+      <div className="flex items-center gap-4">
+        {/* theme menu */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        {/* user menu */}
+        <DropdownMenu>
+          <DropdownMenuTrigger className="cursor-pointer">
+            <Avatar>
+              <AvatarImage src="https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fHww" />
+              <AvatarFallback>JD</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer">
+              <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer">
+              <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" variant="destructive">
+              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
